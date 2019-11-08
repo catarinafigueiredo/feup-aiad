faf9d27ca5998cf814d953251bc1768e3586f325
Catarina Almeida <catamessenger@hotmail.com>
2019-11-08 00:05:20 +0000

    Improvements

---
+++ UberEats/src/UberEats/ParseFiles.java
@@ -0,0 +1,5 @@
+package UberEats;
+
+public class ParseFiles {
+
+}
---
+++ UberEats/src/UberEats/System.java
@@ -0,0 +1,53 @@
+package UberEats;
+// deve ler os ficheiros de clientes restaurants e drivers
+
+import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public class System {
+
+	public static void main(String[] args) {
+		Runtime rt = Runtime.instance();
+
+		Profile p1 = new ProfileImpl();
+		//p1.setParameter(...);
+		ContainerController mainContainer = rt.createMainContainer(p1);
+		
+		Profile p2 = new ProfileImpl();
+		//p2.setParameter(...);
+		ContainerController container = rt.createAgentContainer(p2);
+
+		AgentController ac1;
+		try {
+			ac1 = mainContainer.acceptNewAgent("name1", new Agent()); // por o tipo de agente
+			ac1.start();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
+
+		Object[] agentArgs = new Object[0];
+		AgentController ac2;
+		try {
+			ac2 = container.createNewAgent("name2", "jade.core.Agent", agentArgs);
+			ac2.start();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
+
+		AgentController ac3;
+		try {
+			ac3 = mainContainer.acceptNewAgent("myRMA", new jade.tools.rma.rma());
+			ac3.start();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
